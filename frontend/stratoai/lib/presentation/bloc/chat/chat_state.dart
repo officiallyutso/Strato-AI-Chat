fa71e89ae28a359f63b19e1615b2442ec451c6a2
officiallyutso <utsosarkar1@gmail.com>
2025-06-03 14:19:00 +0530

    feat: chat state

---
+++ frontend/stratoai/lib/presentation/bloc/chat/chat_state.dart
@@ -0,0 +1,41 @@
+part of 'chat_bloc.dart';
+
+abstract class ChatState extends Equatable {
+  const ChatState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class ChatInitial extends ChatState {}
+
+class ChatLoading extends ChatState {}
+
+class ChatLoaded extends ChatState {
+  final Chat chat;
+
+  const ChatLoaded(this.chat);
+
+  @override
+  List<Object> get props => [chat];
+}
+
+class ChatHistoryLoading extends ChatState {}
+
+class ChatHistoryLoaded extends ChatState {
+  final List<Chat> chats;
+
+  const ChatHistoryLoaded(this.chats);
+
+  @override
+  List<Object> get props => [chats];
+}
+
+class ChatError extends ChatState {
+  final String message;
+
+  const ChatError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}

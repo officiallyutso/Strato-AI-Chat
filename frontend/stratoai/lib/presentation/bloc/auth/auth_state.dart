80fa577332ae2aa4f954a8dea5f60dd22c566d38
officiallyutso <utsosarkar1@gmail.com>
2025-06-06 00:35:36 +0530

    update: auth state bloc

--- frontend/stratoai/lib/presentation/bloc/auth/auth_state.dart
+++ frontend/stratoai/lib/presentation/bloc/auth/auth_state.dart
@@ -1,32 +1,13 @@
-part of 'auth_cubit.dart';
-
-abstract class AuthState extends Equatable {
-  const AuthState();
-
-  @override
-  List<Object> get props => [];
-}
+abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
-
 class AuthLoading extends AuthState {}
-
 class Authenticated extends AuthState {
-  final User user;
-
-  const Authenticated(this.user);
-
-  @override
-  List<Object> get props => [user];
+  final String userId;
+  Authenticated(this.userId);
 }
-
 class Unauthenticated extends AuthState {}
-
 class AuthError extends AuthState {
   final String message;
-
-  const AuthError(this.message);
-
-  @override
-  List<Object> get props => [message];
+  AuthError(this.message);
 }

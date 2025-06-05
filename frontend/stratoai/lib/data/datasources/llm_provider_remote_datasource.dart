f0d4197fd6ad83a5da75122316d99160a3d95325
officiallyutso <utsosarkar1@gmail.com>
2025-06-06 00:34:08 +0530

    update: llm provider datasource

--- frontend/stratoai/lib/data/datasources/llm_provider_remote_datasource.dart
+++ frontend/stratoai/lib/data/datasources/llm_provider_remote_datasource.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/llm_provider_model.dart';
+import '../../domain/entities/llm_provider.dart';
 
 class LlmProviderRemoteDataSource {
   final String baseUrl;
@@ -8,15 +9,17 @@ class LlmProviderRemoteDataSource {
 
   LlmProviderRemoteDataSource({required this.baseUrl, required this.client});
 
-  Future<List<LlmProviderModel>> getProviders() async {
+  Future<List<LlmProvider>> getProviders() async {
     final response = await client.get(
-      Uri.parse('$baseUrl/providers'),
+      Uri.parse('$baseUrl/models'), // Changed from /providers to /models
       headers: {'Content-Type': 'application/json'},
     );
 
     if (response.statusCode == 200) {
       final List<dynamic> jsonResponse = jsonDecode(response.body);
-      return jsonResponse.map((json) => LlmProviderModel.fromJson(json)).toList();
+      return jsonResponse
+          .map((json) => LLMModel.fromJson(json).toEntity())
+          .toList();
     } else {
       throw Exception('Failed to get providers');
     }
